b32b55d78c080f4d446668b8c7d1379501b4d361
khaled0y <altarhonikhaled@gmail.com>
2022-12-11 15:25:37 +0200

    fix

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,6 +1,7 @@
 $(function() {
     $(".burger-menu").click(function() {
-        $(".menu-content").slideToggle().toggleClass("active");
+        $(".menu-content").slideToggle();
+        $(this).toggleClass("active");
     });
     $(".container section *, .menu-content a").click(function() {
         $(".menu-content").hide("slow").removeClass("active")
